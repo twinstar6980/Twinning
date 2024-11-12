5d3d4005c9fb3d21900b32f926383049f08d2878
Haruma-VN <114157573+Haruma-VN@users.noreply.github.com>
2024-11-13 02:00:16 +0700

    24-11-13
    Script 116

--- Script/main.ts
+++ Script/main.ts
@@ -2,7 +2,7 @@ namespace Twinning.Script {
 
 	// ------------------------------------------------
 
-	export const k_version = '115';
+	export const k_version = '116';
 
 	// ------------------------------------------------
 
