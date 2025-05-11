7f59dd52c431cf4556bc939b1ceb73039e10e69d
TwinStar <twinstar6980@outlook.com>
2025-05-11 13:49:35 +0800

    25-05-11 Assistant 69

--- Assistant/android/app/build.gradle.kts
+++ Assistant/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.twinstar.twinning.assistant"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.0.13004108"
+    ndkVersion = "28.1.13356709"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- Assistant/lib/common.dart
+++ Assistant/lib/common.dart
@@ -15,7 +15,7 @@ typedef IconSymbols = Symbols;
 
 const String kApplicationName = 'Twinning Assistant';
 
-const String kApplicationVersion = '68';
+const String kApplicationVersion = '69';
 
 // ----------------
 
